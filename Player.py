1d271424d38abf6235bdd6264738652cda80f902
AgusNeira <a2002neira@gmail.com>
2021-10-12 17:46:08 -0300

    [ADD] Sequential orthogonal movements, that can be chained and have a smooth ease-in-out transition

--- Player.py
+++ Player.py
@@ -1,13 +1,10 @@
 import pygame
+from Movement import Movement
 
 # A basic arrow controlled player, represented by a rectangle.
 # Moves orthogonally
 
 class Player:
-    NORTH = lambda x, y, step: (x, y - step)
-    EAST = lambda x, y, step: (x + step, y)
-    SOUTH = lambda x, y, step: (x, y + step)
-    WEST = lambda x, y, step: (x - step, y)
 
     def isOutOfBounds(pos, size, bounds):
         if bounds == None:
@@ -22,24 +19,40 @@ class Player:
 
     def __init__(self, **kwargs):
         self.x, self.y = kwargs.get("position", (0, 0))
-        self.width, self.height = kwargs.get("size", (10, 10))
+        self.width, self.height = kwargs["size"]
         self._speed = kwargs.get("speed", 5)
         self._color = kwargs.get("color", pygame.Color(255, 255, 255))
         self._boundaries = kwargs.get("boundaries", None)
-        self._step = kwargs.get('step', 10)
+        self._step = kwargs['step']
 
-        self._movement = None
+        self.moves_queue = []
 
         self.surface = pygame.Surface((self.width, self.height))
         self.surface.fill(self.color)
 
 
     def tick(self):
-        if self.movement:
-            self.position = self.movement(*self.position, self.step)
+        if len(self.moves_queue) > 0:
+            self.position = self.moves_queue[0].tick()
 
     def draw(self, surface):
         surface.blit(self.surface, self.position)
+
+    def enqueueMovement(self, direction):
+        nmovement = Movement(self._step, direction, end = self.dequeueMovement)
+        self.moves_queue.append(nmovement)
+        if len(self.moves_queue) == 1:
+            self.moves_queue[0].start(self.position)
+
+    def dequeueMovement(self):
+        nextMoveStart = self.moves_queue[0].endPosition()
+        del self.moves_queue[0]
+        if len(self.moves_queue) > 0:
+            self.moves_queue[0].start(nextMoveStart)
+
+    @property
+    def size(self):
+        return self.width, self.height
 
     @property
     def position(self):
@@ -56,28 +69,6 @@ class Player:
             self.x, self.y = npos
         else:
             raise ValueError("New position out of bounds")
-
-    @property
-    def size(self):
-        return self.width, self.height
-    @size.setter
-    def size(self, nwidth, nheight):
-        if not Player.isOutOfBounds(self.position, (nwidth, nheight), self.boundaries):
-            self.width, self.height = nwidth, nheight
-            del self.surface
-            self.surface = pygame.Surface((self.width, self.height))
-            self.surface.fill(self.color)
-        else:
-            raise ValueError("New size set Player out of bounds")
-    @size.setter
-    def size(self, nsize):
-        if not Player.isOutOfBounds(self.position, nsize, self.boundaries):
-            self.width, self.height = nsize
-            del self.surface
-            self.surface = pygame.Surface((self.width, self.height))
-            self.surface.fill(self.color)
-        else:
-            raise ValueError("New size set Player out of bounds")
 
     @property
     def speed(self):
@@ -101,23 +92,3 @@ class Player:
     @property
     def boundaries(self):
         return self._boundaries
-    @boundaries.setter
-    def boundaries(self, nboundaries):
-        if not Player.isOutOfBounds(self.position, self.size, self.boundaries):
-            self._boundaries = nboundaries
-        else:
-            raise ValueError("New boundaries must contain the Player")
-
-    @property
-    def step(self):
-        return self._step
-    @step.setter
-    def step(self, nstep):
-        self._step = nstep
-
-    @property
-    def movement(self):
-        return self._movement
-    @movement.setter
-    def movement(self, nmove):
-        self._movement = nmove
--- main.py
+++ main.py
@@ -1,36 +1,34 @@
 import sys, pygame
 from Player import Player
+from Movement import Movement
 
 pygame.init()
 
-size = width, height = 600, 400
+screenSize = width, height = 600, 400
 speed = 2, 2
 black = pygame.Color(0, 0, 0)
 white = pygame.Color(255, 255, 255)
 
-screen = pygame.display.set_mode(size)
+screen = pygame.display.set_mode(screenSize)
 screen.fill(black)
 
-player = Player()
-player.position = 7.5, 7.5
-player.size = 15, 15
+player = Player(size = (25, 25), boundaries = screenSize, step = 50)
+player.position = 12.5, 12.5
 player.speed = 5
 player.color = 0, 0, 0
-player.boundaries = width, height
-player.step = 5
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                player.movement = Player.NORTH
+                player.enqueueMovement(Movement.NORTH)
             elif event.key == pygame.K_DOWN:
-                player.movement = Player.SOUTH
+                player.enqueueMovement(Movement.SOUTH)            
             elif event.key == pygame.K_LEFT:
-                player.movement = Player.WEST
+                player.enqueueMovement(Movement.WEST)
             elif event.key == pygame.K_RIGHT:
-                player.movement = Player.EAST
+                player.enqueueMovement(Movement.EAST)
         elif event.type == pygame.KEYUP:
             player.movement = None
     player.tick()
@@ -39,5 +37,5 @@ while 1:
     player.draw(screen)
 
     pygame.display.flip()
-    pygame.time.delay(20)
+    pygame.time.delay(10)
 
