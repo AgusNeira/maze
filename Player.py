f9c891ba0813a154529517ff796bc711dfc53510
AgusNeira <a2002neira@gmail.com>
2021-10-12 15:15:22 -0300

    [ADD] Basic continuous movement controlled by arroy keys

--- Player.py
+++ Player.py
@@ -1,30 +1,58 @@
 import pygame
 
 # A basic arrow controlled player, represented by a rectangle.
+# Moves orthogonally
 
 class Player:
+    NORTH = lambda x, y, step: (x, y - step)
+    EAST = lambda x, y, step: (x + step, y)
+    SOUTH = lambda x, y, step: (x, y + step)
+    WEST = lambda x, y, step: (x - step, y)
+
+    def isOutOfBounds(pos, size, bounds):
+        if bounds == None:
+            return False
+        if pos[0] < 0 or pos[1] < 0:
+            return True
+        if pos[0] + size[0] > bounds[0]:
+            return True
+        if pos[1] + size[1] > bounds[1]:
+            return True
+        return False
+
     def __init__(self, **kwargs):
         self.x, self.y = kwargs.get("position", (0, 0))
         self.width, self.height = kwargs.get("size", (10, 10))
-        self.speed = kwargs.get("speed", [5, 5])
+        self._speed = kwargs.get("speed", 5)
         self._color = kwargs.get("color", pygame.Color(255, 255, 255))
         self._boundaries = kwargs.get("boundaries", None)
+        self._step = kwargs.get('step', 10)
+
+        self._movement = None
 
         self.surface = pygame.Surface((self.width, self.height))
         self.surface.fill(self.color)
+
+
+    def tick(self):
+        if self.movement:
+            self.position = self.movement(*self.position, self.step)
+
+    def draw(self, surface):
+        surface.blit(self.surface, self.position)
 
     @property
     def position(self):
         return self.x, self.y
     @position.setter
     def position(self, nx, ny):
-        if self.boundaries is None or (nx + self.witdh <= self.boundaries[0] and ny + self.height <= self.boundaries[1]):
+        if not Player.isOutOfBounds((nx, ny), self.size, self.boundaries):
             self.x, self.y = nx, ny
         else:
             raise ValueError("New position out of bounds")
     @position.setter
     def position(self, npos):
-        if self.boundaries is None or (npos[0] + self.width <= self.boundaries[0] and npos[1] + self.height <= self.boundaries[1]):
+        if not Player.isOutOfBounds(npos, self.size, self.boundaries):
             self.x, self.y = npos
         else:
             raise ValueError("New position out of bounds")
@@ -34,8 +62,17 @@ class Player:
         return self.width, self.height
     @size.setter
     def size(self, nwidth, nheight):
-        if self.boundaries and self.x + nwidth <= self.boundaries[0] and self.y + nheight <= self.boundaries[1]:
+        if not Player.isOutOfBounds(self.position, (nwidth, nheight), self.boundaries):
             self.width, self.height = nwidth, nheight
+            del self.surface
+            self.surface = pygame.Surface((self.width, self.height))
+            self.surface.fill(self.color)
+        else:
+            raise ValueError("New size set Player out of bounds")
+    @size.setter
+    def size(self, nsize):
+        if not Player.isOutOfBounds(self.position, nsize, self.boundaries):
+            self.width, self.height = nsize
             del self.surface
             self.surface = pygame.Surface((self.width, self.height))
             self.surface.fill(self.color)
@@ -57,20 +94,30 @@ class Player:
         if isinstance(ncolor, pygame.Color):
             self._color = ncolor
             self.surface.fill(self.color)
+        elif type(ncolor) == 'tuple':
+            self._color = pygame.Color(*rgb)
+            self.surface.fill(self.color)
 
     @property
     def boundaries(self):
         return self._boundaries
     @boundaries.setter
     def boundaries(self, nboundaries):
-        if self.x + self.width <= nboundaries[0] and self.y + self.height <= nboundaries[1]:
+        if not Player.isOutOfBounds(self.position, self.size, self.boundaries):
             self._boundaries = nboundaries
         else:
             raise ValueError("New boundaries must contain the Player")
 
-    def tick(self):
-        self.position = [pos + speed for (pos, speed) in zip(self.position, self.speed)]
+    @property
+    def step(self):
+        return self._step
+    @step.setter
+    def step(self, nstep):
+        self._step = nstep
 
-    def draw(self, surface):
-        surface.blit(self.surface, self.position)
-
+    @property
+    def movement(self):
+        return self._movement
+    @movement.setter
+    def movement(self, nmove):
+        self._movement = nmove
--- main.py
+++ main.py
@@ -11,13 +11,28 @@ white = pygame.Color(255, 255, 255)
 screen = pygame.display.set_mode(size)
 screen.fill(black)
 
-player = Player(size=(20, 20), speed=(2, 2), color=white)
+player = Player()
+player.position = 7.5, 7.5
+player.size = 15, 15
+player.speed = 5
+player.color = 0, 0, 0
 player.boundaries = width, height
+player.step = 5
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                player.movement = Player.NORTH
+            elif event.key == pygame.K_DOWN:
+                player.movement = Player.SOUTH
+            elif event.key == pygame.K_LEFT:
+                player.movement = Player.WEST
+            elif event.key == pygame.K_RIGHT:
+                player.movement = Player.EAST
+        elif event.type == pygame.KEYUP:
+            player.movement = None
     player.tick()
 
     screen.fill(black)
