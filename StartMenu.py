cdb7c6f9da341be7a8a844129eef7059b70f5452
AgusNeira <a2002neira@gmail.com>
2021-11-02 05:16:35 -0300

    [ADD] Minimal start page with cursor and options to start and exit

---
+++ StartMenu.py
@@ -0,0 +1,59 @@
+import pygame
+
+class StartMenu:
+    def __init__(self, size):
+        self.size = size
+        pygame.font.init()
+
+        self.surface = pygame.Surface(self.size)
+        bigFont = pygame.font.Font('./Early GameBoy.ttf', 32)
+        font = pygame.font.Font('./Early GameBoy.ttf', 18)
+        white = pygame.Color(255, 255, 255)
+
+        self.title = bigFont.render('The Maze', False, white)
+        self.titlePos = self.centerHorizontally(self.title), self.size[1] // 4
+
+        self.menuOptions = [
+                {'name': 'Play', 'code': 1},
+                {'name': 'Exit', 'code': 2}
+        ]
+        self.initOptions(font, self.size[1] * 2.5 // 4, 24)
+        self.currOption = 0
+
+        self.cursor = {
+                'surface': font.render('~', False, white)
+        }
+        self.updateCursorPosition()
+
+    def initOptions(self, font, startHeight, lineHeight):
+        for index, option in enumerate(self.menuOptions):
+            option['surface'] = font.render(option['name'], False, pygame.Color(255, 255, 255))
+            option['position'] = self.centerHorizontally(option['surface']), startHeight + lineHeight * index
+
+    def draw(self, surface):
+        self.surface.fill(pygame.Color(0, 0, 0))
+        self.surface.blit(self.title, self.titlePos)
+        for option in self.menuOptions:
+            self.surface.blit(option['surface'], option['position'])
+        self.surface.blit(self.cursor['surface'], self.cursor['position'])
+        surface.blit(self.surface, (0,0))
+
+    def listen(self, event):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.currOption = (self.currOption - 1) % len(self.menuOptions)
+                self.updateCursorPosition()
+            elif event.key == pygame.K_DOWN:
+                self.currOption = (self.currOption + 1) % len(self.menuOptions)
+                self.updateCursorPosition()
+            elif event.key == pygame.K_RETURN:
+                return self.menuOptions[self.currOption]['code']
+        return 0
+
+    def centerHorizontally(self, txtSurface):
+        return (self.size[0] - txtSurface.get_width()) / 2
+
+    def updateCursorPosition(self):
+        x, y = self.menuOptions[self.currOption]['position']
+        x -= 30
+        self.cursor['position'] = x, y
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys, pygame
 from Player import Player
 from Movement import Movement
 from Maze import Maze
+from StartMenu import StartMenu
 
 pygame.init()
 
@@ -13,18 +14,21 @@ white = pygame.Color(255, 255, 255)
 screen = pygame.display.set_mode(screenSize, pygame.FULLSCREEN | pygame.SCALED)
 screen.fill(black)
 
+startMenu = StartMenu(screenSize)
 maze = Maze(10, 10, screenSize)
 
 while 1:
     for event in pygame.event.get():
         maze.listen(event)
+        print(startMenu.listen(event))
         if event.type == pygame.QUIT: sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE: sys.exit()
 
-    maze.tick()
+    #maze.tick()
     screen.fill(black)
-    maze.draw(screen)
+    #maze.draw(screen)
+    startMenu.draw(screen)
 
     pygame.display.flip()
     pygame.time.delay(10)
